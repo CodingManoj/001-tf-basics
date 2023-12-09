dd4b545ae68395042fe9db1a86bd3a13660e92dd
CodingManoj <CodingManoj@gmail.com>
2023-12-08 23:25:14 -0500

    TF Auto Push

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -2,6 +2,17 @@ variable "fruits" {
     default = [ "apple" , "pineapple" ]
 }
 
+# works
+# output "fruits_op" {
+#     value = var.fruits[1]
+# }
+
+# But this throws as error, index out of exception, that's where functions help
+# output "fruits_op" {
+#     value = var.fruits[2]
+# }
+
 output "fruits_op" {
-    value = var.fruits[2]
+    # value = var.fruits[2]
+    value =  element(var.fruits,1 )
 }
