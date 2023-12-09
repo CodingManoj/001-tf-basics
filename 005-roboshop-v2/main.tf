98f4a6b93dc1cebe170cdb78d34394ecaee1ca93
CodingManoj <CodingManoj@gmail.com>
2023-12-08 23:50:02 -0500

    TF Auto Push

--- 005-roboshop-v2/main.tf
+++ 005-roboshop-v2/main.tf
@@ -2,13 +2,20 @@ variable "components" {
     default = ["mongogd" , "frontend" , "catalogue"]
 }
 
-resource "aws_instance" "instance" {
-  count         = length(var.components)
+# resource "aws_instance" "instance" {
+#   count         = length(var.components)
 
-  ami           = "ami-0f75a13ad2e340a58"
-  instance_type = "t2.micro"
+#   ami           = "ami-0f75a13ad2e340a58"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = element(var.components, count.index)
-  }
+#   tags = {
+#     Name = element(var.components, count.index)
+#   }
+# }
+
+
+resource "aws_security_group" "sg" {
+  count = length(var.components)
+  name  = element(values.components, count.index)
 }
+
