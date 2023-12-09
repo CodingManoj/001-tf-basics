33b83cfaa66b819ae2560bad2594d587ea057d8d
CodingManoj <CodingManoj@gmail.com>
2023-12-09 00:23:22 -0500

    TF Auto Push

--- 006-roboshop-v2/main.tf
+++ 006-roboshop-v2/main.tf
@@ -16,11 +16,11 @@ variable "zoneid" {
 
 variable "components" {
     default = {
-        cart        = { name = "cart-qa" }
-        catalogue   = { name = "catalogue-qa"}
-        payment     = { name = "payment-qa" }
-        shipping    = { name = "shipping-qa"}
-        dispatch    = { name = "dispatch-qa"}
+        cart      = { name = "cart-qa" }
+        catalogue = { name = "catalogue-qa"}
+        payment   = { name = "payment-qa" }
+        shipping  = { name = "shipping-qa"}
+        dispatch  = { name = "dispatch-qa"}
 
     }
 }
@@ -36,24 +36,19 @@ resource "aws_instance" "instance" {
    }
 }
 
-resource "aws_route53_record" "www" {
-  for_each = var.components
+# resource "aws_route53_record" "www" {
+#   for_each = var.components
 
-  zone_id  = var.zoneid
-  name     = "${lookup(each.value, "name", null)}.roboshop.internal"
-  type     = "A"
-  ttl      = 10
-  records  = aws_instance.instance.private_ip
-}
+#   zone_id  = var.zoneid
+#   name     = "${lookup(each.value, "name", null)}.roboshop.internal"
+#   type     = "A"
+#   ttl      = 10
+#   records  = aws_instance.instance.private_ip
+# }
 
 output "instances" {
     value = aws_instance.instance
 }
-
-output "prv_ip" {
-    value = aws_instance.instance[each.key].private_ip
-}
-
 
 resource "aws_security_group" "sg" {
   for_each = var.components
