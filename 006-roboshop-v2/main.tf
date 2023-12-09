d06d9aef0ced7817bb29bf8e54e94ca0c133e4cb
CodingManoj <CodingManoj@gmail.com>
2023-12-09 00:10:36 -0500

    TF Auto Push

---
+++ 006-roboshop-v2/main.tf
@@ -0,0 +1,40 @@
+variable "ami" {
+  default = "ami-0f75a13ad2e340a58"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "sgid" {
+  default = "sg-052fd946b7e11841a"
+}
+
+variable "components" {
+    default = {
+        cart        = { name = "cart-qa" }
+        catalogue   = { name = "catalogue-qa" },
+        payment     = { name = "payment-qa" },
+        shipping    = { name = "shipping-qa" },
+        dispatch    = { name = "dispatch-qa" },
+
+    }
+}
+
+resource "aws_instance" "instance" {
+  for_each         = var.components  
+
+  ami              = var.ami 
+  instance_type    = var.instance_type
+
+  tags = {
+        Name       = lookup(each.value, "name", null)
+   }
+}
+
+
+
+resource "aws_security_group" "sg" {
+  for_each = var.components
+  name     = lookup(var.components, each.value["name"], null)
+}
