a911262ef23a788b7b45a03e121c7d4667023bec
CodingManoj <CodingManoj@gmail.com>
2023-12-10 16:03:54 -0500

    TF Auto Push

---
+++ 07-module/local-module/ec2.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "DevOps-LabImage-CentOS7"
+  owners      = ["355449129696"]
+}
+
+output "ami" {
+  value = data.aws_ami.ami
+}
+
+# output "private_ip" {
+#     value   =  aws_instance.instance.private_ip
+# }
+
+
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["sg-052fd946b7e11841a"]
+}
+
+variable "instance_type" {}
---
+++ 07-module/main.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+    default =  t2.micro
+}
+
+module "ec2" {
+    source          = "./local-module"
+    instance_type   = var.instance_type
+}
+
+output "ami" {
+    value = module.test
+}
