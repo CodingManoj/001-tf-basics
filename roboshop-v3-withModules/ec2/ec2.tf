a6d343337b5f41774ce3a1584bb8addcc369c228
CodingManoj <CodingManoj@gmail.com>
2023-12-10 17:20:03 -0500

    TF Auto Push

--- roboshop-v3-withModules/ec2/ec2.tf
+++ roboshop-v3-withModules/ec2/ec2.tf
@@ -15,12 +15,11 @@ resource "aws_instance" "instance" {
     }
 }
 
-# resource "aws_route53_record" "www" {
-#   for_each = var.components
+resource "aws_route53_record" "www" {
 
-#   zone_id  = var.zoneid
-#   name     = "${lookup(each.value, "name", null)}.roboshop.internal"
-#   type     = "A"
-#   ttl      = 10
-#   records  = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
-# }
+  zone_id  = var.zone_id
+  name     = var.name
+  type     = "A"
+  ttl      = 10
+  records  = [aws_instance.instance.private_ip]
+}
