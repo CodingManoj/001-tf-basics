9795979771ac98e66ac77e7eaea5490c37454324
CodingManoj <CodingManoj@gmail.com>
2023-12-10 19:12:22 -0500

    TF Auto Push

--- roboshop-v3-withModules/ec2/ec2.tf
+++ roboshop-v3-withModules/ec2/ec2.tf
@@ -63,7 +63,7 @@ resource "null_resource" "app" {
         "sleep 30",
         "COMPONENT=${var.name}",
         "COMP=$(echo ${var.name} | sed 's/-dev//g')",
-        "ansible-pull -U https://github.com/b56-clouddevops/ansible.git -e ENV=dev -e COMPONENT=$COMP roboshop-pull.yml"
+        "ansible-pull -U https://github.com/b56-clouddevops/ansible.git -e ENV=dev -e COMPONENT=$COMP -e MYSQL_PSW="RoboShop@1" roboshop-pull.yml"
     ]
   }
 }
